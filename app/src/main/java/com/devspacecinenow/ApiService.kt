d592b4860a4501deadab244b5fb7c66fe1f7d693
Jonas Santos <josan.png@gmail.com>
2026-03-17 08:32:13 -0100

    Reorganizing the app files, creating navigation controller and setting up a detail screen for the movies

--- app/src/main/java/com/devspacecinenow/ApiService.kt
+++ app/src/main/java/com/devspacecinenow/ApiService.kt
@@ -6,5 +6,14 @@ import retrofit2.http.GET
 interface ApiService {
 
     @GET("now_playing?language=en-US&page=1")
-    fun getNowPlayingMovies() : Call<MovieResponse>
+    fun getNowPlayingMovies(): Call<MovieResponse>
+
+    @GET("top_rated?language=en-US&page=1")
+    fun getTopRatedMovies(): Call<MovieResponse>
+
+    @GET("popular?language=en-US&page=1")
+    fun getPopularMovies(): Call<MovieResponse>
+
+    @GET("upcoming?language=en-US&page=1")
+    fun getUpcomingMovies(): Call<MovieResponse>
 }
